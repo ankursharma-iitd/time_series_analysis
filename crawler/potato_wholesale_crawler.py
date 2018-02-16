c4de133ebc235000f79928ea02145dd794e59390
Ankur Sharma <ankur.sharma127@gmail.com>
2018-02-16 16:57:35 +0530

    cdv

--- crawler/potato_wholesale_crawler.py
+++ crawler/potato_wholesale_crawler.py
@@ -31,8 +31,8 @@ def monthtodays(month,year):
         return 31
 
 months1 = ["January","February","March","April","May","June","July","August","September","October","November","December"]
-# months2 = ["January","February","March","April","May","June","July","August","September","October","November"]
-# months3 = ["June","July","August","September","October","November","December"]
+centers = ["LUCKNOW", "AGRA", "BARAUT", "VARANASI", "KANPUR"]
+
 
 def month_name_to_number(number):
     if number == "January":
@@ -70,57 +70,46 @@ def extractdata():
     print "1"
     myfile= open('mynewdata.csv','a')
     for center in centernames:
-        start_year = 2015
-        end_year = 2017
-        for year in range(start_year,end_year+1):
-            months = months1
-            # if(year == 2017):
-            # 	months = months2
-            # elif(year == 2015):
-            # 	months = months3
-            for month in months:
-                print year,month
-                browser.get(url)
-                browser.implicitly_wait(30)
-                browser.find_element_by_xpath("//*[@id=\"ctl00_ContentPlaceHolder1_ddlyear\"]/option[contains(text(),\""+str(year)+"\")]").click()
-                browser.find_element_by_xpath("//*[@id=\"ctl00_ContentPlaceHolder1_ddlmonth\"]/option[contains(text(),\""+month+"\")]").click()
-                browser.implicitly_wait(30)
-                browser.find_element_by_xpath("//*[@id=\"ctl00_ContentPlaceHolder1_drpCategoryName\"]/option[contains(text(),\""+"VEGETABLES"+"\")]").click()
-                browser.implicitly_wait(30)
-                browser.find_element_by_xpath("//*[@id=\"ctl00_ContentPlaceHolder1_drpCropName\"]/option[contains(text(),\""+"POTATO"+"\")]").click()
-                browser.implicitly_wait(30)
-                browser.find_element_by_xpath("//*[@id=\"ctl00_ContentPlaceHolder1_ddlvariety\"]/option[contains(text(),\""+"POTATO FRESH"+"\")]").click()
-                browser.implicitly_wait(30)
-                browser.find_element_by_xpath("//*[@id=\"ctl00_ContentPlaceHolder1_LsboxCenterList\"]/option[contains(text(),\""+"AGRA"+"\")]").click()
-                browser.implicitly_wait(30)
-                browser.find_element_by_xpath("//*[@id=\"ctl00_ContentPlaceHolder1_btnSearch\"]").click()
-                table = browser.find_element_by_xpath("//*[@id=\"ctl00_ContentPlaceHolder1_GridViewmonthlypriceandarrivalreport\"]")
-                rows = table.find_elements_by_tag_name("tr")
-                monthnum = month_name_to_number(month)
-                start_dt = date(year,monthnum,1)
-                end_dt = date(year, monthnum, monthtodays(month,year))
-                dates = givedates(start_dt,end_dt)
-                myfile= open('mynewretaildata.csv','a')
-                cells = rows[1].find_elements_by_xpath(".//*[local-name(.)='td']")
-                temp = [cell.text for cell in cells]
-                for i in range(0,len(dates)):
-                    temp1 = temp[i+4].split()
-                    if temp1 != []:
-                        mystr=dates[i]+','+temp1[3]+'\n'
-                    else:
-                        mystr=dates[i]+',0'+'\n'
-                    myfile.write(mystr)
-                myfile.close()
+        for retail_name in centers:
+            start_year = 2006
+            end_year = 2006
+            for year in range(start_year,end_year+1):
+                months = months1
+                for month in months:
+                    print year,month
+                    browser.get(url)
+                    browser.implicitly_wait(30)
+                    browser.find_element_by_xpath("//*[@id=\"ctl00_ContentPlaceHolder1_ddlyear\"]/option[contains(text(),\""+str(year)+"\")]").click()
+                    browser.find_element_by_xpath("//*[@id=\"ctl00_ContentPlaceHolder1_ddlmonth\"]/option[contains(text(),\""+month+"\")]").click()
+                    browser.implicitly_wait(30)
+                    browser.find_element_by_xpath("//*[@id=\"ctl00_ContentPlaceHolder1_drpCategoryName\"]/option[contains(text(),\""+"VEGETABLES"+"\")]").click()
+                    browser.implicitly_wait(30)
+                    browser.find_element_by_xpath("//*[@id=\"ctl00_ContentPlaceHolder1_drpCropName\"]/option[contains(text(),\""+"POTATO"+"\")]").click()
+                    browser.implicitly_wait(30)
+                    browser.find_element_by_xpath("//*[@id=\"ctl00_ContentPlaceHolder1_ddlvariety\"]/option[contains(text(),\""+"POTATO FRESH"+"\")]").click()
+                    browser.implicitly_wait(30)
+                    browser.find_element_by_xpath("//*[@id=\"ctl00_ContentPlaceHolder1_LsboxCenterList\"]/option[contains(text(),\""+retail_name+"\")]").click()
+                    browser.implicitly_wait(30)
+                    browser.find_element_by_xpath("//*[@id=\"ctl00_ContentPlaceHolder1_btnSearch\"]").click()
+                    table = browser.find_element_by_xpath("//*[@id=\"ctl00_ContentPlaceHolder1_GridViewmonthlypriceandarrivalreport\"]")
+                    rows = table.find_elements_by_tag_name("tr")
+                    monthnum = month_name_to_number(month)
+                    start_dt = date(year,monthnum,1)
+                    end_dt = date(year, monthnum, monthtodays(month,year))
+                    dates = givedates(start_dt,end_dt)
+                    myfilename = 'retaildata/mynewretaildata'+retail_name+'.csv'
+                    myfile= open(myfilename,'a')
+                    cells = rows[1].find_elements_by_xpath(".//*[local-name(.)='td']")
+                    temp = [cell.text for cell in cells]
+                    for i in range(0,len(dates)):
+                        temp1 = temp[i+4].split()
+                        if temp1 != []:
+                            mystr=dates[i]+','+temp1[3]+'\n'
+                        else:
+                            mystr=dates[i]+',0'+'\n'
+                        myfile.write(mystr)
+                    myfile.close()
 
 
 if __name__ == '__main__':
     extractdata()
-
-
-'''
-<span id="ctl00_ContentPlaceHolder1_GridViewmonthlypriceandarrivalreport_ctl02_lblresult" 
-style="display:inline-block;width:98%;">
-3000<br>4800<br>4000<br>5000<br>1596<br>
-</span>
-
-'''
